1a53d1ac36c6151739a7ab61a54b764d3359e583
dayoung87 <donna0807@naver.com>
2026-01-21 17:47:14 +0900

    [Solve] 28691 정보보호학부 동아리 소개

---
+++ codes/28691.py
@@ -0,0 +1,14 @@
+# 28691 정보보호학부 동아리 소개
+
+word = input()
+
+if word == "M":
+    print("MatKor")
+elif word == "W":
+    print("WiCys")
+elif word == "C":
+    print("CyKor")
+elif word == "A":
+    print("AlKor")
+elif word == "$":
+    print("$clear")
